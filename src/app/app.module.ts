88a0357e4fa0c7d342c5bb9051f5314883f24d0c
wifekatt <wifek.attia.2000@gmail.com>
2022-04-10 15:52:17 +0100

    changeess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 import { CondidateListComponent } from './condidate-list/condidate-list.component';
 import { UsersComponent } from './users/users.component';
 
@@ -41,7 +42,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatDividerModule,
     MatListModule,
-
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
