2af708fc2823222182a7a53b76fd686801048312
wifekatt <wifek.attia.2000@gmail.com>
2022-04-18 17:48:00 +0100

    admin add

--- src/app/admins-dialog/admins-dialog.component.ts
+++ src/app/admins-dialog/admins-dialog.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { AdminApiService } from '../services/admin-api.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-admins-dialog',
   templateUrl: './admins-dialog.component.html',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminsDialogComponent implements OnInit {
 
-  constructor() { }
+  AdminsForm !: FormGroup;
+  actionBtn : string = "Save";
+
+  constructor(private formBuilder : FormBuilder ,
+    private adminApi : AdminApiService ,
+    @Inject(MAT_DIALOG_DATA) public editData : any,
+    private dialogRef : MatDialogRef<AdminsDialogComponent>) { }
+
 
   ngOnInit(): void {
+    this.AdminsForm = this.formBuilder.group({
+      FullName : ['',Validators.required],
+      Password : ['',Validators.required],
+      PrivilegeID : ['',Validators.required],
+      })
+      if(this.editData){
+        this.actionBtn = "Update";
+        this.AdminsForm.controls['FullName'].setValue(this.editData.FullName);
+        this.AdminsForm.controls['Password'].setValue(this.editData.Password);
+        this.AdminsForm.controls['PrivilegeID'].setValue(this.editData.PrivilegeID);
+      }
+}
+addAdmin(){
+  if(!this.editData){
+    if(this.AdminsForm.valid){
+      this.adminApi.postAdmin(this.AdminsForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("user added successfully")
+          this.AdminsForm.reset();
+          this.dialogRef.close('save');
+        },
+        error:()=>{
+          alert("error while adding the user!!")
+        }
+      })
+    }
+  }else{
+    this.updateAdmin();
   }
 
+}
+
+updateAdmin(){
+  this.adminApi.putAdmin(this.AdminsForm.value,this.editData.id)
+  .subscribe({
+    next:(res)=>{
+      alert("user updated successfully")
+      this.AdminsForm.reset();
+      this.dialogRef.close('update');
+    },
+    error:()=>{
+      alert("error while updating the user!!")
+    }
+  })
+}
 }
--- src/app/admins/admins.component.ts
+++ src/app/admins/admins.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { AdminsDialogComponent } from '../admins-dialog/admins-dialog.component';
+import { AdminApiService } from '../services/admin-api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
@@ -7,9 +12,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminsComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['FullName', 'Password', 'PrivilegeID'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog , private adminApi : AdminApiService) {
+
+  }
 
   ngOnInit(): void {
+    this.getAllAdmins();
+  }
+
+  openDialog() {
+    this.dialog.open(AdminsDialogComponent, {
+      width:'30%'
+    }).afterClosed().subscribe(val=>{
+      if(val === 'save'){
+        this.getAllAdmins();
+        }
+    })
+  }
+
+  getAllAdmins(){
+    this.adminApi.getAdmin()
+          .subscribe({
+            next:(res)=>{
+              this.dataSource = new MatTableDataSource(res);
+              this.dataSource.paginator = this.paginator;
+              this.dataSource.sort = this.sort;
+
+            },
+            error:(err)=>{
+              alert("error while fetching the records!!")
+            }
+          })
+  }
+
+  editAdmin(row : any){
+    this.dialog.open(AdminsDialogComponent,{
+    width:'30%',
+    data:row
+    }).afterClosed().subscribe(val=>{
+      if(val === 'update'){
+        this.getAllAdmins();
+        }
+    })
+    }
+  deleteAdmin(id : number){
+    this.adminApi.deleteAdmin(id)
+    .subscribe({
+      next:(res)=>{
+        alert("admin deleted successfully");
+        this.getAllAdmins();
+      },
+      error:()=>{
+        alert("error while deleting the admin!!")
+      }
+    })
+
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
